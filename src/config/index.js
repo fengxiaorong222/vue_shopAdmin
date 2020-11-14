4afe2a0e5d5d94ede9babaf35c432f04b8305d2c
fengxiaorong222 <fxrdsg@163.com>
2020-11-14 21:44:49 +0800

    更新接口地址

--- src/config/index.js
+++ src/config/index.js
@@ -2,7 +2,7 @@ export default {
     title: 'admin',
     baseUrl: {
         //开发后台接口地址
-        dev: 'http://timemeetyou.com:8889/api/private/v1/',
+        dev: 'http://47.113.99.149:8888/api/private/v1/',
         //上线后台接口地址
         pro: ''
     }
